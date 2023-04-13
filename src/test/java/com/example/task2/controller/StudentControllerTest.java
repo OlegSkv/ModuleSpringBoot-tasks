93a4868f0b55ccf46beef21b7fce18c8dc620c38
Oleg_Skvortcov <oleg_skvortcov@epam.com>
2023-04-13 20:32:11 +0300

    Task 7 REST testing

---
+++ src/test/java/com/example/task2/controller/StudentControllerTest.java
@@ -0,0 +1,237 @@
+package com.example.task2.controller;
+
+import com.example.task2.exception.StudentNotFoundException;
+import com.example.task2.model.Student;
+import com.example.task2.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class StudentControllerTest {
+
+    public static final String ID = "id";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String ERROR_MESSAGE = "error";
+
+    @MockBean
+    MessageSource messageSource;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentService studentService;
+
+    @Test
+    void newStudent_shouldReturnCreatedStudent() throws Exception {
+        given(studentService.create(any(Student.class))).willReturn(new Student(1, "name", "surname"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/students")
+                        .content(asJsonString(new Student(0, "name", "surname")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ID).value(1))
+                .andExpect(jsonPath(FIRST_NAME).value("name"))
+                .andExpect(jsonPath(LAST_NAME).value("surname"));
+    }
+
+    @Test
+    void newStudent_shouldReturnBadRequestWithMessageWhenMandatoryFieldIsNull() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/students")
+                        .content("{ \"firstName\" : null, \"lastName\" : \"surname\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath(ERROR_MESSAGE).exists());
+
+        verify(messageSource, times(1)).getMessage(any(), any(), any(), any());
+    }
+
+    @Test
+    void newStudent_shouldReturnBadRequestWithMessageWhenMandatoryFieldIsAbsent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/students")
+                        .content("{ \"lastName\" : \"surname\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath(ERROR_MESSAGE).exists());
+    }
+
+    @Test
+    void newStudent_shouldReturnBadRequestWithMessageWhenMandatoryFieldIsEmpty() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/students")
+                        .content("{ \"firstName\" : \"\", \"lastName\" : \"surname\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath(ERROR_MESSAGE).exists());
+    }
+
+    @Test
+    void newStudent_shouldReturnBadRequestWithMessageWhenTwoValidationErrorsExist() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/students")
+                        .content("{ \"firstName\" : \"\", \"wrongFieldName\" : \"surname\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath(ERROR_MESSAGE).exists());
+
+        verify(messageSource, times(2)).getMessage(any(), any(), any(), any());
+    }
+
+    @Test
+    void getAllStudents_shouldReturnAllStudents() throws Exception {
+        given(studentService.getAll()).willReturn(List.of(
+                new Student(2, "name2", "surname2"),
+                new Student(1, "name1", "surname1")));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[*]." + ID, containsInAnyOrder(1, 2)))
+                .andExpect(jsonPath("$[*]." + FIRST_NAME, containsInAnyOrder("name1", "name2")))
+                .andExpect(jsonPath("$[*]." + LAST_NAME, containsInAnyOrder("surname1", "surname2")));
+    }
+
+    @Test
+    void getAllStudents_shouldReturnEmptyCollectionWhenNoStudents() throws Exception {
+        given(studentService.getAll()).willReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void getStudent_shouldReturnStudentById() throws Exception {
+        long studentId = 1;
+        given(studentService.get(studentId)).willReturn(new Student(studentId, "name", "surname"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/students/" + studentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ID).value(studentId))
+                .andExpect(jsonPath(FIRST_NAME).value("name"))
+                .andExpect(jsonPath(LAST_NAME).value("surname"));
+    }
+
+    @Test
+    void getStudent_shouldReturnBadRequestWithMessageWhenStudentDoesNotExist() throws Exception {
+        long wrongStudentId = 1;
+        given(studentService.get(wrongStudentId)).willThrow(new StudentNotFoundException("Error message"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/students/" + wrongStudentId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ERROR_MESSAGE).value("Error message"));
+    }
+
+    @Test
+    void updateStudent_shouldReturnUpdatedStudent() throws Exception {
+        long studentId = 1;
+        given(studentService.update(any(Student.class)))
+                .willReturn(new Student(studentId, "updated name", "updated surname"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/students/" + studentId)
+                        .content(asJsonString(new Student(studentId, "updated name", "updated surname")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ID).value(studentId))
+                .andExpect(jsonPath(FIRST_NAME).value("updated name"))
+                .andExpect(jsonPath(LAST_NAME).value("updated surname"));
+    }
+
+    @Test
+    void updateStudent_shouldReturnBadRequestWithBodyWhenStudentDoesNotExist() throws Exception {
+        ArgumentCaptor<Student> studentCaptor = ArgumentCaptor.forClass(Student.class);
+        long wrongStudentId = 1;
+        given(studentService.update(any(Student.class)))
+                .willThrow(new StudentNotFoundException("Error message"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/students/" + wrongStudentId)
+                        .content(asJsonString(new Student(0, "updated name", "updated surname")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ERROR_MESSAGE).value("Error message"));
+
+        verify(studentService).update(studentCaptor.capture());
+        Student passedStudent = studentCaptor.getValue();
+        assertThat(passedStudent.getId()).isEqualTo(wrongStudentId);
+    }
+
+    @Test
+    void updateStudent_shouldReturnBadRequestWithBodyWhenValidationErrorsExist() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/students/1")
+                        .content("{ \"firstName\" : \"\", \"lastName\" : \"surname\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath(ERROR_MESSAGE).exists());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/example/task2/service/StudentServiceImplTest.java
@@ -0,0 +1,143 @@
+package com.example.task2.service;
+
+import com.example.task2.data.StudentRepository;
+import com.example.task2.exception.StudentNotFoundException;
+import com.example.task2.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceImplTest {
+
+    @Mock
+    private MessageSource messageSource;
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    private StudentServiceImpl studentService;
+
+    @BeforeEach
+    void setUp() {
+        studentService = new StudentServiceImpl(messageSource, studentRepository);
+    }
+
+    @Test
+    void create_shouldSaveStudent() {
+        studentService.create(new Student(1, "firstName", "lastName"));
+        verify(studentRepository).save(any(Student.class));
+    }
+
+    @Test
+    void getAll_shouldFindAllStudents() {
+        studentService.getAll();
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    void get_shouldFindStudentById() {
+        ArgumentCaptor<Long> studentIdCapture = ArgumentCaptor.forClass(Long.class);
+        long studentId = 3;
+
+        given(studentRepository.findById(studentId)).willReturn(Optional.of(new Student()));
+
+        studentService.get(studentId);
+
+        verify(studentRepository).findById(studentIdCapture.capture());
+        assertThat(studentIdCapture.getValue()).isEqualTo(studentId);
+    }
+
+    @Test
+    void get_shouldThrowExceptionIfStudentDoesNotExist() {
+        long wrongStudentId = 4;
+        given(studentRepository.findById(wrongStudentId)).willReturn(Optional.empty());
+        given(messageSource.getMessage(any(), any(), any())).willReturn("Error message");
+
+        assertThatThrownBy(() -> studentService.get(wrongStudentId))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Error message");
+
+        verify(studentRepository).findById(anyLong());
+        verify(messageSource).getMessage(any(), any(), any());
+    }
+
+    @Test
+    void update_shouldUpdateValidStudent() {
+        Student updatedStudent = new Student(2, "updated name", "updated surname");
+        Student originalStudent = new Student(2, "original name", "original surname");
+        ArgumentCaptor<Student> studentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        given(studentRepository.findById(updatedStudent.getId())).willReturn(Optional.of(originalStudent));
+        given(studentRepository.save(any(Student.class))).willReturn(updatedStudent);
+
+        studentService.update(updatedStudent);
+
+        verify(studentRepository).findById(anyLong());
+        verify(studentRepository).save(studentCaptor.capture());
+        Student passedStudent = studentCaptor.getValue();
+        assertThat(passedStudent.getId()).isEqualTo(originalStudent.getId());
+        assertThat(passedStudent.getFirstName()).isEqualTo(updatedStudent.getFirstName());
+        assertThat(passedStudent.getLastName()).isEqualTo(updatedStudent.getLastName());
+    }
+
+    @Test
+    void update_shouldThrowStudentNotFoundExceptionOnInvalidStudent() {
+        long wrongStudentId = 4;
+        given(studentRepository.findById(wrongStudentId)).willReturn(Optional.empty());
+        given(messageSource.getMessage(any(), any(), any())).willReturn("Error message");
+
+        assertThatThrownBy(() -> studentService.update(new Student(wrongStudentId, "name", "surname")))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Error message");
+
+        verify(studentRepository).findById(anyLong());
+        verify(studentRepository, never()).save(any(Student.class));
+    }
+
+    @Test
+    void delete_shouldDeleteExistingStudent() {
+        ArgumentCaptor<Long> studentIdCaptor = ArgumentCaptor.forClass(Long.class);
+        given(studentRepository.existsById(anyLong())).willReturn(true);
+
+        long studentId = 1L;
+        studentService.delete(studentId);
+
+        verify(studentRepository).existsById(studentIdCaptor.capture());
+        verify(studentRepository).deleteById(studentIdCaptor.capture());
+        List<Long> passedStudentIds = studentIdCaptor.getAllValues();
+
+        assertThat(passedStudentIds).allMatch(id -> id == studentId);
+    }
+
+    @Test
+    void delete_shouldThrowStudentNotFoundExceptionIfStudentDoesNotExist() {
+        ArgumentCaptor<Long> studentIdCaptor = ArgumentCaptor.forClass(Long.class);
+        given(studentRepository.existsById(anyLong())).willReturn(false);
+        given(messageSource.getMessage(any(), any(), any())).willReturn("Error message");
+
+        long studentId = 1L;
+
+        assertThatThrownBy(() -> studentService.delete(studentId))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Error message");
+
+        verify(studentRepository).existsById(anyLong());
+        verify(messageSource).getMessage(any(), any(), any());
+    }
+}
