e4f1db17d4282572c4ff9f0c872274755c50a5e4
Oleg_Skvortcov <oleg_skvortcov@epam.com>
2023-04-13 19:38:06 +0300

    Task 1 CommandLineRunner

--- src/main/java/com/example/task2/Task2Application.java
+++ src/main/java/com/example/task2/Task2Application.java
@@ -1,12 +1,21 @@
 package com.example.task2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Task2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task2Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run() {
+		return args -> {
+			System.out.println("hello world");
+		};
 	}
 }
